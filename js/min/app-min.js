41be1220fdd853b23ad126552113f7154e1e9c54
Matteo <matteo.verolla@gmail.com>
2016-03-14 14:30:35 +0100

    Migliorato il menu
    -Migliorato alcuni aspetti del menu
    -Pagina Homepage quasi terminata

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,1 @@
-var main=function(){$(".icon-menu").click(function(){$(".menu").animate({left:"0px"},200)}),$(".icon-close").click(function(){$(".menu").animate({left:"-285px"},200)})};$(document).ready(main);
+var main=function(){$(".menu-icon").click(function(){$(".menu").animate({left:"0px"},200)}),$(".icon-close").click(function(){$(".menu").animate({left:"-285px"},200)})};$(document).ready(main);
